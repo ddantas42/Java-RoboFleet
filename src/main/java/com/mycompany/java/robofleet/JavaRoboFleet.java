036d35fce60d3a81252250e84ca29f35a7b11f7d
estriga-001 <rogerenes123@gmail.com>
2025-12-03 18:42:39 +0000

    Adicionar classes todas vazias

---
+++ src/main/java/com/mycompany/java/robofleet/JavaRoboFleet.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.java.robofleet;
+
+/**
+ *
+ * @author hiper
+ */
+public class JavaRoboFleet {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
