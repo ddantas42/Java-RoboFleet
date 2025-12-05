41cd9b0a33f1735fc7852a96484e5778edfdf458
Miguel <SHUTUP@M.com>
2025-12-05 12:46:51 +0000

    Menu completo

--- src/main/java/com/mycompany/java/robofleet/Gestao/Menu.java
+++ src/main/java/com/mycompany/java/robofleet/Gestao/Menu.java
@@ -11,11 +11,11 @@ import java.util.Scanner;
  * @author migue
  */
 public class Menu {
-    private Scanner opcao;
+    private Scanner sc;
 
     // construtor
-    public Menu(Scanner opcao){
-        this.opcao = opcao;
+    public Menu(Scanner sc){
+        this.sc = sc;
     }
 
     public void menuInicialString(){
@@ -26,8 +26,16 @@ public class Menu {
             System.out.println("(1) Gerir tecnicos"); // criar, editar, remover, listar, associar/desassociar
             System.out.println("(2) Gerir Robots"); // criar, editar, remover, listar
             System.out.println("(3) Gerir zonas");
-            System.out.println("(4)  ");
-        }
+            System.out.println("(4) Gerir complexo");
+            System.out.println("(5) Utilizar radar");
+            System.out.println("(6) Exportar dados");
+            System.out.println("(0) Sair");
+            System.out.println("Opcao: ");
+            
+            opcao = sc.nextInt();
+            sc.nextLine(); // consume \n
+
+        } while(opcao != 0);
     }
 
 }
