08f9c69126f838f4f16bf457f84cc7ca4ec86cb0
Diogo <diogosousasilva.silva@gmail.com>
2025-12-10 15:11:15 +0000

    RFactory

--- src/main/java/com/mycompany/java/robofleet/Robot/RFactory.java
+++ src/main/java/com/mycompany/java/robofleet/Robot/RFactory.java
@@ -4,10 +4,28 @@
  */
 package com.mycompany.java.robofleet.Robot;
 
-/**
- *
- * @author migue
- */
-public class RFactory {
-    
+public class RFactory extends Robot
+{
+
+    private int numBracosRoboticos;
+    private boolean temOrientacaoLaser;
+
+    public RFactory(String nome, String marca, String modelo, int ano, Zona zona, Bateria bateria, int numBracos)
+    {
+        super(nome, marca, modelo, ano, zona, bateria);
+
+        if (zona != Zona.LINHA_PROD_1 && zona != Zona.LINHA_PROD_2 && zona != Zona.ESTACAO_CARGA)
+        {
+            throw new IllegalArgumentException("R-Factory só pode operar nas Linhas de Produção.");
+        }
+
+        this.numBracosRoboticos = numBracos;
+        this.temOrientacaoLaser = true;
+    }
+
+    @Override
+    public boolean validarEquipa()
+    {
+        return true;
+    }
 }
