3b9caca39aff7003e4a55ef1de557c7ee5a72f55
Diogo <diogosousasilva.silva@gmail.com>
2025-12-10 15:01:53 +0000

    RCarry

--- src/main/java/com/mycompany/java/robofleet/Robot/RCarry.java
+++ src/main/java/com/mycompany/java/robofleet/Robot/RCarry.java
@@ -4,10 +4,39 @@
  */
 package com.mycompany.java.robofleet.Robot;
 
-/**
- *
- * @author migue
- */
-public class RCarry {
-    
+public class RCarry extends Robot
+{
+
+    private double capacidadeCarga;
+    private boolean temHolofote;
+
+    public RCarry(String nome, String marca, String modelo, int ano, Zona zona, Bateria bateria, double capacidadeCarga, boolean temHolofote)
+    {
+        super(nome, marca, modelo, ano, zona, bateria);
+        this.capacidadeCarga = capacidadeCarga;
+        this.temHolofote = temHolofote;
+    }
+
+    public void adicionarMotor(Motor m)
+    {
+        if (motores.size() < 4)
+        {
+            motores.add(m);
+        } else
+        {
+            System.out.println("Erro: RCarry só pode ter até 4 motores.");
+        }
+    }
+
+    @Override
+    public boolean validarEquipa()
+    {
+        return true; 
+    }
+
+    @Override
+    public String toString()
+    {
+        return super.toString() + " [Tipo: R-Carry] Carga: " + capacidadeCarga;
+    }
 }
